1a9503c74247416fd8b2b66d0a7d46f11be6b9f3
gabriel <gaboubastard@gmail.com>
2016-10-17 16:01:55 -0700

    3rd search on linkedin scraping done

--- lkdn.py
+++ lkdn.py
@@ -71,58 +71,64 @@ emailRegex = re.compile(r'''(
 def get_profile_info(driver, url):
     driver.get(url)
     infos = {}
-    infos['name'] = driver.find_element_by_class_name('full-name').text
-    infos['title'] = driver.find_element_by_class_name('title').text
-    infos['current_employer'] =  driver.find_element_by_id('overview-summary-current').find_element_by_tag_name('td').find_element_by_tag_name('a').text
-    infos['previous_employer'] =  driver.find_element_by_id('overview-summary-past').find_element_by_tag_name('td').find_element_by_tag_name('a').text
-    infos['colleges']=[]
-    colleges = driver.find_element_by_id('background-education').find_elements_by_class_name('editable-item')
-    for college in colleges:
-        college_infos = {}
-        college_infos['college_name'] = college.find_element_by_class_name('summary').text
-        college_infos['degree'] = college.find_element_by_class_name('degree').text
-        try:        
-            college_infos['major'] = college.find_element_by_class_name('major').text
-        except NoSuchElementException:
-            pass
-        try:
-            college_infos['degree'] = college.find_element_by_class_name('degree').text
-        except NoSuchElementException:
-            pass
-
-        times = college.find_element_by_class_name('education-date').find_elements_by_tag_name('time')
-        if times:        
-            college_infos['starting_date'] = times[0].text
-            college_infos['ending_date'] = times[1].text[2::]
+    try:
+        infos['name'] = driver.find_element_by_class_name('full-name').text
+        infos['title'] = driver.find_element_by_class_name('title').text
+        infos['current_employer'] =  driver.find_element_by_id('overview-summary-current').find_element_by_tag_name('td').find_element_by_tag_name('a').text
+        infos['previous_employer'] =  driver.find_element_by_id('overview-summary-past').find_element_by_tag_name('td').find_element_by_tag_name('a').text
+        infos['colleges']=[]
+        colleges = driver.find_element_by_id('background-education').find_elements_by_class_name('editable-item')
+        for college in colleges:
+            college_infos = {}
+            college_infos['college_name'] = college.find_element_by_class_name('summary').text
+            try:
+                college_infos['degree'] = college.find_element_by_class_name('degree').text   
+            except NoSuchElementException:
+                pass
+            try:        
+                college_infos['major'] = college.find_element_by_class_name('major').text
+            except NoSuchElementException:
+                pass
+    
+            times = college.find_element_by_class_name('education-date').find_elements_by_tag_name('time')
+            if times:        
+                college_infos['starting_date'] = times[0].text
+                if len(times)>1:
+                    college_infos['ending_date'] = times[1].text[2::]
             infos['colleges'].append(college_infos)
+    except NoSuchElementException:
+        print("profile incomplete")
     infos['profile_link'] = url[:url.index('?')]
-    
-    phones = []
-    text = driver.find_element_by_id('profile').text
-    for groups in phoneRegex.findall(text):
-        phoneNum = '-'.join([groups[1], groups[3], groups[5]])
-        if groups[8] != '':
-            phoneNum += ' x' + groups[8]
-            phones.append(phoneNum)
-    if len(phones) > 0:
-        infos['email'] = ', '.join(phones)
-    
-    emails=[]
-    for groups in emailRegex.findall(text):
-        emails.append(groups[0])
-    if len(emails) > 0:
-        infos['email'] = ', '.join(emails)
+
+    try:    
+        phones = []
+        text = driver.find_element_by_id('profile').text
+        for groups in phoneRegex.findall(text):
+            phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+            if groups[8] != '':
+                phoneNum += ' x' + groups[8]
+                phones.append(phoneNum)
+        if len(phones) > 0:
+            infos['email'] = ', '.join(phones)
+        
+        emails=[]
+        for groups in emailRegex.findall(text):
+            emails.append(groups[0])
+        if len(emails) > 0:
+            infos['email'] = ', '.join(emails)
+    except NoSuchElementException:
+        print('weird profile')
         
     
     return(infos)
 
-def scrape_all_profiles(driver, url_profiles):
+def scrape_all_profiles(driver, url_profiles, start):
     profiles_info_file = open('profiles_info.txt','w')
     count = 0
-    success_count=0
-    for url in url_profiles:
+    success_count = 0
+    for i in range(start,len(url_profiles)):
+        url=url_profiles[i]
         count+=1
-#        try:
         driver.get(url)
         try:
             WebDriverWait(driver, 2).until(
@@ -130,7 +136,16 @@ def scrape_all_profiles(driver, url_profiles):
             )            
             print("Page is ready!", count)
         except TimeoutException:
-            print("Loading took too much time!")
+            try:
+                WebDriverWait(driver, 2).until(
+                    EC.presence_of_element_located((By.ID, "pagekey-uas-consumer-login-internal"))
+                ) 
+                print('got caught')
+                return(start+count)
+            except TimeoutException:
+                print("Page was too long to load")
+                pass
+#                linkedin_connect(driver, 'h2827476@mvrht.com','azedsq')
         new_url = driver.current_url
         if 'www.linkedin.com/in/' in new_url:
             print("public profile")
@@ -140,12 +155,45 @@ def scrape_all_profiles(driver, url_profiles):
             print('added infos to file')
             success_count += 1
             print('sucesses :', success_count)
-#        except:
-#            print('private profile', count)
-#            pass
 
 #%%
 profiles_infos=[]
 driver = webdriver.Firefox()
-linkedin_connect(driver, 'h2816176@mvrht.com','azedsq')
-scrape_all_profiles(driver,url_profiles)
+linkedin_connect(driver, 'h3005628@mvrht.com','azedsq')
+scrape_all_profiles(driver, url_profiles, 2060)
+profiles_infos_no_dup=[i for n, i in enumerate(profiles_infos) if i not in profiles_infos[n + 1:]]
+
+#%% let's remove useless colleges
+
+for person in profiles_infos_no_dup:
+    try:
+        chosen_college = person['colleges'][0]
+        person.update(chosen_college)
+        del person['colleges']
+    except IndexError:
+        del person['colleges']
+        pass
+    except KeyError:
+        pass
+            
+
+
+
+#%%
+import csv
+
+
+#keys=profiles_infos_no_dup[23].keys()
+keys = ['name', 'title', 'current_employer', 'email', 'previous_employer', 'profile_link', 'college_name', 'starting_date', 'ending_date', 'major', 'degree']
+with open('profiles_info.csv', 'w') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(profiles_infos_no_dup)
+
+#%%
+
+
+
+
+
+
